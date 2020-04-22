fbf46851b6cf9d8edc849a52c5bd1e778ca319f5
Konstantin Loginov <kostghost@gmail.com>
2020-04-22 23:57:23 +0500

    add collisions

---
+++ Assets/Scripts/CollideWithEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using UnityEngine;
+
+public class CollideWithEnemy : MonoBehaviour
+{
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Возможно, эту штуку нужно инвертировать
+        if (collision.gameObject.tag == Tags.SIMPLE_ENEMY)
+        {
+            // ух
+            GetComponent<Transform>().position = GetComponent<HeroController>().startPosition;
+        }  
+    }
+}
--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -4,20 +4,26 @@ using System.Dynamic;
 using System.Numerics;
 using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class HeroController : MonoBehaviour
 {
     public float shiftSpeed;
     public float runSpeed;
+    public Vector3 startPosition;
+
 
     private Rigidbody2D rb;
 
     private Vector2 moveSpeed;
     private Vector2 heroPositionOnScreen;
 
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.position = startPosition;
+
         heroPositionOnScreen = GetHeroPositionOnScreen();
     }
 
