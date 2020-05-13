69bc3cf7af98e2b5273c05d70c1bde88c37a5f51
Konstantin Loginov <kostghost@gmail.com>
2020-05-13 21:54:30 +0500

    add music settings

---
+++ Assets/Scripts/settings/MusicSettings.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicSettings : MonoBehaviour
+{
+    public AudioMixerGroup mixer;
+
+    public void ToggleMusic(bool enabled)
+    {
+        toggle("MusicVolume", enabled);
+    }
+
+    public void ToggleSfx(bool enabled)
+    {
+        toggle("EffectsVolume", enabled);
+    }
+
+    private void toggle(string mixerName ,bool enabled)
+    {
+        if (enabled)
+        {
+            mixer.audioMixer.SetFloat(mixerName, 0);
+        }
+        else
+        {
+            mixer.audioMixer.SetFloat(mixerName, -80);
+        }
+    }
+}
