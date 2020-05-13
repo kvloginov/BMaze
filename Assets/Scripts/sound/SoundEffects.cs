d149db3a3728a9708efb8037207acc55c4e1deb2
Konstantin Loginov <kostghost@gmail.com>
2020-05-13 22:46:44 +0500

    add sound effects

---
+++ Assets/Scripts/sound/SoundEffects.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundEffects : MonoBehaviour
+{
+    public AudioMixerGroup mixer;
+    public bool makeSoundQuieterOnEnable;
+    public AudioMixerSnapshot normalSnapshot;
+    public AudioMixerSnapshot quietSnapshot;
+
+    private void OnEnable()
+    {
+        if (makeSoundQuieterOnEnable)
+        {
+            quietSnapshot.TransitionTo(0.3f);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (makeSoundQuieterOnEnable)
+        {
+            normalSnapshot.TransitionTo(0.3f);
+        }
+    }
+}
